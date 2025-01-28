9760b9e0ba1e9e4d43a1a2b8eb094cdf11852fb2
Guilherme webber <guiz1n.webber@gmail.com>
2025-01-28 15:59:53 -0300

    to-do form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,30 +2,30 @@ import React from "react";
 
 function Form() {
   return (
-    <div className="bg-zinc-200 p-10 m-10">
+    <div className="bg-zinc-200 p-10 m-10 rounded-lg">
       <form action="POST">
         <input
           type="text"
           placeholder="Título da tarefa"
-          className="p-2 my-3 w-full"
+          className="p-2 my-3 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-lime-500 "
         />
         <textarea
           placeholder="Descrição da tarefa"
-          className="p-2 my-3 w-full"
+          className="p-2 my-3 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-lime-500"
         />
-        <select name="prioridade" id="prioridade" className="w-full my-3 p-2">
+        <select name="prioridade" id="prioridade" className="w-full my-3 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-lime-500">
           <option value="">--Prioridade--</option>
           <option value="alta">Alta prioridade</option>
           <option value="media">Média prioridade</option>
           <option value="baixa">Baixa prioridade</option>
         </select>
-        <select name="categoria" id="categoria" className="w-full my-3 p-2">
+        <select name="categoria" id="categoria" className="w-full my-3 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-lime-500">
           <option value="">--Categoria--</option>
           <option value="estudos">Estudos</option>
           <option value="trabalho">Trabalho</option>
           <option value="pessoal">Pessoal</option>
         </select>
-        <input type="date" className="w-full my-3 p-2"/>
+        <input type="date" className="w-full p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-lime-500 "/>
       </form>
     </div>
   );
